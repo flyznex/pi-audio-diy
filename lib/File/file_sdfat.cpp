56e37accb0f9e8d2ce0332d8e80417d3b0d6803e
thuanpt <thuan.pham@sunteco.io>
2024-06-05 17:44:07 +0700

    continue with unimplement method

--- lib/File/file_sdfat.cpp
+++ lib/File/file_sdfat.cpp
@@ -1,7 +1,91 @@
-#include<file_sdfat.h>
+#include <file_sdfat.h>
 #include "SdFat.h"
 
-static bool init(u_int8_t cs) {
-    if (!sd.begin(SD_CONFIG)) return false;
+static bool initCard()
+{
+    if (!sd.begin(SD_CONFIG))
+        return false;
     return root.open("/");
+}
+
+// indexing file in dir
+static void indexFiles(const char *path)
+{
+    file_t dirFile;
+    if (!dirFile.open(path, O_RDONLY))
+    {
+        // TODO: print to display
+        Serial.println("Failed to open directory");
+        return;
+    }
+    dirFile.rewind();
+    // TODO: can check time via RTC, set TTL to refresh file.
+    // or set function to force reindex.
+    String idxFileName = String(path) + "/" + INDEX_DB_FILE_NAME;
+    sd.remove(idxFileName.c_str()); // check
+    if (!idxFile.open(idxFileName.c_str()), O_RDWR | O_CREAT)
+    {
+        Serial.println("Failed to create temp file");
+        return;
+    }
+    // scan folder and insert value to index file.
+    // format:
+    // prefix|<child-directory-name>
+    // prefix|<filename.ext>
+    // prefix = 0 is directory
+    // prefix = 1 is file
+    while (dirFile.openNext(&file, O_RDONLY))
+    {
+        // check is directory
+        String rc;
+        if (file.isDir())
+        {
+            String fname = file.FileName();
+            indexFiles(fname);
+            rc = addPrefix(INDEX_ROW_PREFIX_DIR, fname);
+        }
+        else
+        {
+            String fname = file.fileName();
+            rc = addPrefix(INDEX_ROW_PREFIX_FILE,fname));
+        }
+        addRecord(idxFile, rc);
+        file.close();
+    }
+    dirFile.close();
+    idxFile.close();
+}
+
+String addPrefix(String prefix, String s)
+{
+    return prefix + s;
+}
+
+void addRecord(Print *pr, String rc)
+{
+    pr->write(rc.c_str(), rc.length());
+    pr->write("\n");
+}
+
+static void readingIndex(const char *dirPath)
+{
+    String tempFilePath = String(dirPath) + "/" + INDEX_DB_FILE_NAME;
+    file_t tempFile;
+    if (!tempFile.open(tempFilePath.c_str(), O_RDONLY))
+    {
+        Serial.println("Failed to open index file");
+        return;
+    }
+    fileCount = 0;
+    //tempFile.fgets()
+    while (tempFile.available())
+    {
+        char ch;
+        tempFile.read(&ch, 1);
+        if (ch == '\n')
+        {
+            fileCount++;
+        }
+    }
+    tempFile.close();
 }
--- lib/File/file_sdfat.h
+++ lib/File/file_sdfat.h
@@ -6,26 +6,34 @@
 
 #define SPI_CLOCK SD_SCK_MHZ(4)
 
+#define INDEX_DB_FILE_NAME ".index"
+#define INDEX_ROW_PREFIX_FILE "1|"
+#define INDEX_ROW_PREFIX_DIR "0|"
+const size_t LINE_DIM = 50;
+
 #if SD_FAT_TYPE == 0
-SdFat sd;
-File file;
-File root;
+typedef SdFat sd_t;
+typedef File file_t;
 #elif SD_FAT_TYPE == 1
-SdFat32 sd;
-File32 file;
-File32 root;
+typedef SdFat32 sd_t;
+typedef File32 file_t;
 #elif SD_FAT_TYPE == 2
-SdExFat sd;
-ExFile file;
-ExFile root;
+typedef SdExFat sd_t;
+typedef ExFile file_t;
 #elif SD_FAT_TYPE == 3
-SdFs sd;
-FsFile file;
-FsFile root;
+typedef SdFs sd_t;
+typedef FsFile file_t;
 #else  // SD_FAT_TYPE
 #error Invalid SD_FAT_TYPE
 #endif  // SD_FAT_TYPE
+
+sd_t sd;
+file_t file;
+file_t root;
+file_t idxFile;
 const uint8_t SD_CS_PIN = 10;
+int fileCount = 0;
+
 
 // Try to select the best SD card configuration.
 #if HAS_SDIO_CLASS
@@ -36,4 +44,6 @@ const uint8_t SD_CS_PIN = 10;
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
 #endif  // HAS_SDIO_CLASS
 
-bool init(uint8_t cs = SD_CS_PIN);
+bool initCard();
+void indexFiles(const char * path);
+void readingIndex(const char * dirPath);
