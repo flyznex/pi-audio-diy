790f48059d96659519bd77c6b8d8dc2dd722560d
thuanpt <thuan.pham@sunteco.io>
2024-06-10 17:48:52 +0700

    added I2C mode for display

--- pi-audio-diy.ino
+++ pi-audio-diy.ino
@@ -1,30 +1,3 @@
-/*
-   I2S stereo microphone (input) example
-   Run using the Arduino Serial Plotter to see waveform.
-   Released to the Public Domain by Earle F. Philhower, III
-
-   For the Google AIY Voice Hat Microphone daughterboard, part
-   of the Raspberry Pi AIY cardboard box, the I2S stereo pinout
-   looking at the board top with the RPI logo on the left hand
-   side:
-            +--   ------------------------------------  --+
-   left RPI | (1) GND (2) DIN (3) BCLK (4) LRCLK (5) 3.3V | AIY right
-       logo +---------------------------------------------+ logo
-
-   For an Adfruit I2S MEMS microphone (https://www.adafruit.com/product/3421),
-   connect the pins as follows:
-
-      DOUT -> GPIO0
-      BCLK <- GPIO1
-      LRCL <- GPIO2  # LRCLK = BCLK + 1
-      GND <-> GND
-      3V  <-> 3V3OUT
-
-   The other idiosyncrasy of most modern MEMS microphones is that they
-   require a minimum clock rate to wake up. For example, the SPH0645
-   microphone needs at least 1MHz.
-*/
-
 #include "src/Display/ssd1306.h"
 
 void setup() {
--- src/Display/ssd1306.h
+++ src/Display/ssd1306.h
@@ -3,17 +3,23 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SPI.h>
+#include <Wire.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
 // Declaration for SSD1306 display connected using SPI
+#ifdef OLED_SPI
 #define OLED_MOSI 11
 #define OLED_CLK 10
 #define OLED_DC 14
 #define OLED_CS 13
 #define OLED_RESET 12
 static Adafruit_SSD1306 ssd1036Display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI1, OLED_DC, OLED_RESET, OLED_CS);
+#else
+#define OLED_RESET -1
+static Adafruit_SSD1306 ssd1036Display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#endif
 
 bool initDisplay();
 void drawStr(int posX, int posY, int size, String content, bool isInverse = false);
